6f1d76f8e623cf58e5923bdb45d37434f2317df9
kylee.prescott <kylee.prescott@CLC-LAB209-09.TCSD.tooelesd.org>
2018-09-06 14:09:17 -0600

    chapter 2 practice

---
+++ src/Kyleeprescottch2exe/Ch2Exe.java
@@ -0,0 +1,17 @@
+package Kyleeprescottch2exe;
+/*
+ Kylee Prescott
+ 9/18
+ ch2 exe
+ */
+
+public class Ch2Exe {
+
+	public static void main(String[] args) {
+		// display some text 
+		System.out.println("welcome to java" );
+		
+		
+		
+	}
+}
